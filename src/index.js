b3ef86b59d453bce474e7eb020e937f55b20a5db
David Sergey <nirth.furzahad@gmail.com>
2016-06-08 22:21:10 +0200

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-
+// Let's recap basics of the pure functions and differences from closures.
 require('./pure-functions');
+
+// Example of why pure functions are very useful, and map reduce.
 require('./map-reduce');
+
+// Let's create very simple app state, and use reduce to update it as actions arrive.
 require('./simple-app-state');
